b79601dfa28109336368d25d52ccd612c7445a1f
FalinGeorgiy <ivan.ivan.local@mail.ru>
2023-10-04 22:00:13 +0500

    Add project files.

---
+++ Tyuiu.FalinGS.Sprint2.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FalinGS.Sprint2.Task0.V18.Lib;
+
+namespace Tyuiu.FalinGS.Sprint2.Task0.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 5105;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, true, false, true, false, true };
+
+            CollectionAssert.AreEqual(wait, res);
+            
+        }
+    }
+}
---
+++ Tyuiu.FalinGS.Sprint2.Task0.V18/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FalinGS.Sprint2.Task0.V18.Lib;
+
+namespace Tyuiu.FalinGS.Sprint2.Task0.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 5105;
+            int y = 475;
+
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.Title = "Спринт #1 | Выполнил: Фалин Г.С | ИИПб-23-1";
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Cпринт №2                                                               #");
+            Console.WriteLine("* Задание №0                                                          #");
+            Console.WriteLine("* Вариант №18                                                             #");
+            Console.WriteLine("* Выполнил: Фалин Георгий Сергеевич / ИИПб-23-1                           #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Исходные данные:                                                        #");
+            Console.WriteLine("###########################################################################");
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Result:                                                                 #");
+            Console.WriteLine("###########################################################################");
+
+            for (int i=0; i<6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
