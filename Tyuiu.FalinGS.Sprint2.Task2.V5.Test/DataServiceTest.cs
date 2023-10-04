53d267c2be8e0fa8c669594c593034aff9605c00
FalinGeorgiy <ivan.ivan.local@mail.ru>
2023-10-05 00:47:28 +0500

    Task2

---
+++ Tyuiu.FalinGS.Sprint2.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FalinGS.Sprint2.Task2.V5.Lib;
+
+namespace Tyuiu.FalinGS.Sprint2.Task2.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDoInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 3;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.FalinGS.Sprint2.Task2.V5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FalinGS.Sprint2.Task2.V5.Lib;
+
+namespace Tyuiu.FalinGS.Sprint2.Task2.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            Console.Title = "Спринт #1 | Выполнил: Фалин Г.С | ИИПб-23-1";
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Cпринт №2                                                               #");
+            Console.WriteLine("* Задание №2                                                              #");
+            Console.WriteLine("* Вариант №5                                                              #");
+            Console.WriteLine("* Выполнил: Фалин Георгий Сергеевич / ИИПб-23-1                           #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Исходные данные:                                                        #");
+            Console.WriteLine("###########################################################################");
+
+            Console.WriteLine("Введите переменную Х:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите переменную Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Result:                                                                 #");
+            Console.WriteLine("###########################################################################");
+
+
+           if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области!:");
+            }
+           else
+            {
+                Console.WriteLine("Точка находится НЕ в заштрихованной области!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
