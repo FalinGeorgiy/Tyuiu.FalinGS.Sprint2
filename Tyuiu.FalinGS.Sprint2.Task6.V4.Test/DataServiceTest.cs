ba368154e765adc97ab9880128556edc670f986e
FalinGeorgiy <ivan.ivan.local@mail.ru>
2023-10-13 12:33:23 +0500

    Task6

---
+++ Tyuiu.FalinGS.Sprint2.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FalinGS.Sprint2.Task6.V4.Lib;
+
+namespace Tyuiu.FalinGS.Sprint2.Task6.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindCardSuit()
+        {
+            DataService ds = new DataService();
+
+            Assert.AreEqual("Шестерка", ds.FindCardSuit(6));
+            Assert.AreEqual("Семерка", ds.FindCardSuit(7));
+            Assert.AreEqual("Восьмерка", ds.FindCardSuit(8));
+            Assert.AreEqual("Девятка", ds.FindCardSuit(9));
+            Assert.AreEqual("Десятка", ds.FindCardSuit(10));
+            Assert.AreEqual("Валет", ds.FindCardSuit(11));
+            Assert.AreEqual("Дама", ds.FindCardSuit(12));
+            Assert.AreEqual("Король", ds.FindCardSuit(13));
+            Assert.AreEqual("Туз", ds.FindCardSuit(14));
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindCardSuit(5);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindCardSuit(15);
+            });
+        }
+    }
+}
---
+++ Tyuiu.FalinGS.Sprint2.Task6.V4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FalinGS.Sprint2.Task6.V4.Lib;
+
+namespace Tyuiu.FalinGS.Sprint2.Task6.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Фалин Г.С | ИИПб-23-1";
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Cпринт №2                                                               #");
+            Console.WriteLine("* Задание №6                                                              #");
+            Console.WriteLine("* Вариант №4                                                              #");
+            Console.WriteLine("* Выполнил: Фалин Георгий Сергеевич / ИИПб-23-1                           #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Исходные данные:                                                        #");
+            Console.WriteLine("###########################################################################");
+
+            Console.WriteLine("Введите номер карты: ");
+            int numCard = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if (numCard<6 && numCard > 14)
+            {
+                res = "Введено неверное значение";
+            } 
+            else
+            {
+                res = "Эта карта:" + ds.FindCardSuit(numCard);
+            }
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Результат:                                                              #");
+            Console.WriteLine("###########################################################################");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
